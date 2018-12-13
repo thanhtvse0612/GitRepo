6c93c682901566cbeef231acdb0cfc7b1d746bfd
ThanhTV13 <thanhtv13@fsoft.com.vn>
2018-12-14 05:59:49 +0700

    component three

--- SampleProject/src/app/app.module.ts
+++ SampleProject/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ComponentOneComponent } from './component-one/component-one.component';
 import { ComponentTwoComponent } from './component-two/component-two.component';
+import { ComponentThreeComponent } from './component-three/component-three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentOneComponent,
-    ComponentTwoComponent
+    ComponentTwoComponent,
+    ComponentThreeComponent
   ],
   imports: [
     BrowserModule
